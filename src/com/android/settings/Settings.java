c67433dba353c8f06db729b3e35a72a62b45fbd3
maxwen <max.weninger@gmail.com>
2017-08-31 23:16:42 +0800

    Settings: Add SakuraSettings dashboard
    Change-Id: Ie18a29e62a651b4b5c74510493dc291135f5361c
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -488,6 +488,7 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    public static class SakuraSettingsActivity extends SettingsActivity {}
 
     /**
      * Activity for MediaControlsSettings
@@ -498,6 +499,7 @@ public class Settings extends SettingsActivity {
      * Activity for AppDashboard.
      */
     public static class AppDashboardActivity extends SettingsActivity {}
+
 
     public static class AdaptiveBrightnessActivity extends SettingsActivity { /* empty */ }
 
