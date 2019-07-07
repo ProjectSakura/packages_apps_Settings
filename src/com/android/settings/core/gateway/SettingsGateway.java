ea134fdbe885af28e3821257df8d1692f5c08468
Pranav Vashi <neobuddy89@gmail.com>
2019-07-07 11:56:32 +0530

    Fix long click intent for Smart Pixels tile [2/2]
    Change-Id: Id6b5fffd814f035be1bb75d1434456e0c02aa97f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -225,6 +225,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.sakura.settings.SakuraSettings;
+import com.sakura.settings.fragments.ui.SmartPixels;
 
 public class SettingsGateway {
 
@@ -439,6 +440,7 @@ public class SettingsGateway {
             SafetyCenterFragment.class.getName(),
             SakuraSettings.class.getName(),
             RunningServices.class.getName(),
+            SmartPixels.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -489,5 +491,6 @@ public class SettingsGateway {
             Settings.ShadeSettingsActivity.class.getName(),
             Settings.SakuraSettingsActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName(),
     };
 }
