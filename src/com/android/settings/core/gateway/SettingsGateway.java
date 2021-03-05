crdroidandroid/android_packages_apps_Settings

Bring back Gaming mode [2/3]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -149,6 +149,7 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.crdroid.settings.crDroidSettingsLayout;
+import com.crdroid.settings.fragments.misc.GamingMode;
 import com.crdroid.settings.fragments.ui.SmartPixels;
 
 public class SettingsGateway {
@@ -288,6 +289,7 @@ public class SettingsGateway {
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
             crDroidSettingsLayout.class.getName(),
+            GamingMode.class.getName(),
             SmartPixels.class.getName(),
     };
 
@@ -328,6 +330,7 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.crDroidSettingsLayoutActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
     };
 }

ProjectSakura/packages_apps_Settings

Change-Id: Iac1b92118a722c35891feac836667543aa0c3cdc
Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -161,6 +161,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.sakura.settings.SakuraSettings;
+import com.sakura.settings.fragments.misc.GamingMode;
 
 public class SettingsGateway {
 
@@ -310,7 +311,8 @@ public class SettingsGateway {
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
             MediaControlsSettings.class.getName(),
-            SakuraSettings.class.getName()
+            SakuraSettings.class.getName(),
+            GamingMode.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -351,5 +353,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.SakuraSettingsActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
     };
             }